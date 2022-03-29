a5ecda32e867225e4acb49566610cb47a122d1eb
Wafa Johal <wafa.johal@gmail.com>
2022-03-29 15:20:45 +1100

    update pageconfig

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,6 @@ st.set_page_config(
      page_title="Social Drones in Education Analysis App",
      page_icon="🧊",
      layout="wide",
-     initial_sidebar_state="expanded",
-     menu_items={
-         'Get Help': 'https://www.extremelycoolapp.com/help',
-         'Report a bug': "https://www.extremelycoolapp.com/bug",
-         'About': "# This is a header. This is an *extremely* cool app!"
-     }
  )
 
 
